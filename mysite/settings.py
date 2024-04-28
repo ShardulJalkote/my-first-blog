8a4253254d3493e66c5cb982173a26a4dfa164de
shard_odes <sharduljalkote11@gmail.com>
2024-04-28 15:37:36 +0530

    updated the setting file - to fix mime type error for CSS

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
